255c42722ebfb5889c94e5ce99799b5551b954ec
AyushKumarSrivas <117811777+AyushKumarSrivas@users.noreply.github.com>
2024-07-09 20:24:52 +0530

    Add files via upload

---
+++ Assignment 4.c
@@ -0,0 +1,113 @@
+/*1.Write a program to print MySirG 5 times on the screen.
+main()
+{
+    int i;
+    for(i=1;i<=5;i++)
+    {
+        printf("MySirG\n");
+    }
+}
+*/
+
+/*2.Write a program to print the first 10 natural numbers.
+main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i);
+    }
+}
+*/
+
+/*3.Write a program to print the first 10 natural numbers in reverse order.
+main()
+{
+    int i;
+    for(i=10;i>=1;i--)
+    {
+        printf("%d\n",i);
+    }
+}
+*/
+
+/*4.Write a program to print the first 10 odd naturals numbers.
+main()
+{
+    int i;
+    for(i=1;i<=19;i++)
+    {
+        if(i%2!=0)
+            printf("%d\n",i);
+    }
+}
+*/
+
+/*5.Write a program to print the first 10 odd natural numbers in reverse order.
+main()
+{
+    int i;
+    for(i=19;i>=1;i--)
+    {
+        if(i%2!=0)
+            printf("%d\n",i);
+    }
+}
+*/
+
+/*6.Write a program to print the first 10 even natural numbers.
+main()
+{
+    int i;
+    for(i=2;i<=20;i++)
+    {
+        if(i%2==0)
+            printf("%d\n",i);
+    }
+}
+*/
+
+/*7.Write a program to print the first 10 even natural numbers in reverse order.
+main()
+{
+    int i;
+    for(i=20;i>=2;i--)
+    {
+        if(i%2==0)
+            printf("%d\n",i);
+    }
+}
+*/
+
+/*8.Write a program to print the squares of first 10 natural numbers.
+main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i*i);
+    }
+}
+*/
+
+/*9.Write a program to print the cubes of first 10 natural numbers.
+main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i*i*i);
+    }
+}
+*/
+
+//10.Write a program to print table of 5.
+main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("5 * %d = %d\n",i,5 * i);
+    }
+}
+
